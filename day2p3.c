5eed60bf12410a9ed82958676e7865e5c9391216
Vasubhalodi011 <134709721+Vasubhalodi011@users.noreply.github.com>
2023-05-26 19:09:50 +0600

    Add files via upload

---
+++ day2p3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+  int main(){
+  	
+  	printf("  - - -\n");
+  	printf("|\t|\n");
+  	printf("v\t|\n");
+  	printf("a\t|\n");
+  	printf("s\t|\n");
+  	printf("u\t|\n");
+  	printf("|\t|\n");
+  	printf("  - - - \n");
+  	
+  	
+  	return 0;
+	    }
+  	
+  	
+
+
---
+++ day4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+ int main(){
+ 	
+ 	int a,b,sum;
+ 	
+ 	printf("enter first value :");
+ 	scanf("%d",&a);
+ 	printf("enter second value :");
+ 	scanf("%d",&b);
+ 	
+ 	sum = a-b;
+ 	
+ 	printf("sum = %d",sum);
+ 	
+ 	return 0;
+ 	
+ 	}
