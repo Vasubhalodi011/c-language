4c2909f5ff8b43b47490a178f837ff83520bfa95
Vasubhalodi011 <134709721+Vasubhalodi011@users.noreply.github.com>
2023-06-01 18:53:45 +0600

    Add files via upload

---
+++ day8/day8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+  int main(){
+  	
+  	char aryan, meet;
+  	
+  	 printf("aryan marks :");
+  	 scanf("%d", &aryan);
+  	 
+	printf("meet marks :");
+  	 scanf("%d", &meet);
+  	
+  	if (aryan>meet){
+  		printf("aryan marks lower then meet");
+	  }
+  	else{
+  		printf("meet marks higer then aryan");
+	  }
+	   
+	   return 0;
+  }
