24f691a4f7984d2902fc5e4b775d320deb702cb8
Vasubhalodi011 <134709721+Vasubhalodi011@users.noreply.github.com>
2023-06-07 19:32:22 +0600

    Add files via upload

---
+++ day12/day12p4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i=1;
+	int n,sum=0;
+	
+	printf("enter value :");
+	scanf("%d",&n);
+	
+	while(i<=n){
+		
+	   sum  = sum + i;
+	   i ++;
+		
+	}
+	 printf("sum : %d",sum);
+	
+	
+	return 0;
+}
