442dbeac28619a8d2b62a5f189d30f70990066a8
Vasubhalodi011 <134709721+Vasubhalodi011@users.noreply.github.com>
2023-06-14 18:55:35 +0600

    Add files via upload

---
+++ day14/day14p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'a';
+
+    while(ch<='z')
+    {
+        printf("%c\n", ch);
+        ch++;
+    }
+
+    return 0;
+}
+
+
---
+++ day14/day14p2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,n;
+	
+	printf("enter the value :");
+	scanf("%d",&n);
+	
+	while(i<=10)
+	{
+		printf("%d x %d = %d\n",n,i,n*i);
+		i++;
+	}
+	
+	return 0;
+}
+
